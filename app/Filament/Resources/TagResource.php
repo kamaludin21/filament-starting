a5e4e5da607a46f526c56c8281d883e9b29b25ae
kamal <kamal.zyel@gmail.com>
2023-09-11 08:07:57 +0700

    crud tag

---
+++ app/Filament/Resources/TagResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TagResource\Pages;
+use App\Models\Tag;
+use Filament\Forms\Components\Radio;
+use Illuminate\Support\Str;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ToggleColumn;
+
+class TagResource extends Resource
+{
+  protected static ?string $model = Tag::class;
+
+  protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+  public static function form(Form $form): Form
+  {
+    return $form
+      ->schema([
+        Section::make('Form Tag')
+          ->description('Data tag pada artikel berita')
+          ->schema([
+            TextInput::make('name')
+              ->label('Nama tag')
+              ->live(onBlur: true)
+              ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state)))->placeholder('Nama instansi/lembaga')->required()->autocomplete(false),
+            TextInput::make('slug')->required(),
+            Radio::make('is_muted')
+              ->label('Sembunyikan kata tag ini?')
+              ->boolean()->default(false)
+          ])
+      ]);
+  }
+
+  public static function table(Table $table): Table
+  {
+    return $table
+      ->columns([
+        TextColumn::make('name')->sortable()->label('Tag'),
+        ToggleColumn::make('is_muted')->label('Sembunyikan'),
+        TextColumn::make('created_at')->date()
+      ])
+      ->filters([
+        //
+      ])
+      ->actions([
+        Tables\Actions\EditAction::make(),
+      ])
+      ->bulkActions([
+        Tables\Actions\BulkActionGroup::make([
+          Tables\Actions\DeleteBulkAction::make(),
+        ]),
+      ])
+      ->emptyStateActions([
+        Tables\Actions\CreateAction::make(),
+      ]);
+  }
+
+  public static function getRelations(): array
+  {
+    return [
+      //
+    ];
+  }
+
+  public static function getPages(): array
+  {
+    return [
+      'index' => Pages\ListTags::route('/'),
+      'create' => Pages\CreateTag::route('/create'),
+      'edit' => Pages\EditTag::route('/{record}/edit'),
+    ];
+  }
+}
