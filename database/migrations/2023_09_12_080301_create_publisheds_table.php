e39def885d701a922e09e621271075d30d84910b
kamal <kamal.zyel@gmail.com>
2023-09-15 16:51:55 +0700

    published resources

---
+++ database/migrations/2023_09_12_080301_create_publisheds_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Stakeholder;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('publisheds', function (Blueprint $table) {
+      $table->id();
+      $table->foreignIdFor(Stakeholder::class)->constrained();
+      $table->enum('publish_status', [
+        'queue',
+        'preview',
+        'hold',
+        'publish'
+      ])->default('queue');
+      $table->timestamp('publish_date')->nullable();
+      $table->enum('edited_status', [
+        'drafted',
+        'completed',
+        'published',
+        'archived'
+      ]);
+      $table->timestamps();
+      $table->softDeletes();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('publisheds');
+  }
+};
