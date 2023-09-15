f673b6f0bfb9751b763c8f02d6bd7aadde10beb6
kamal <kamal.zyel@gmail.com>
2023-09-15 16:52:24 +0700

    admin panel filament

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers\Filament;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationGroup;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -53,6 +54,11 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->navigationGroups([
+              NavigationGroup::make()->label('Artikel'),
+              NavigationGroup::make()->label('Data Master'),
+              NavigationGroup::make()->label('Pengaturan'),
+          ]);
     }
 }
