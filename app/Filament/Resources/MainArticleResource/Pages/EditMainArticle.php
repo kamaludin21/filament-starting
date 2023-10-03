27eb6c2b87de067a6540730271cbd4443984e6a8
kamal <kamal.zyel@gmail.com>
2023-10-03 12:00:51 +0700

    actions: - get value from tags and category instead id - use current time for modify time

--- app/Filament/Resources/MainArticleResource/Pages/EditMainArticle.php
+++ app/Filament/Resources/MainArticleResource/Pages/EditMainArticle.php
@@ -4,10 +4,10 @@ namespace App\Filament\Resources\MainArticleResource\Pages;
 
 use App\Filament\Resources\MainArticleResource;
 use App\Models\Published;
+use Carbon\Carbon;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
 class EditMainArticle extends EditRecord
 {
@@ -15,15 +15,20 @@ class EditMainArticle extends EditRecord
 
   protected function handleRecordUpdate(Model $record, array $data): Model
   {
+    $data['tags'] = $record->tags->pluck('name')->filter();
+    $data['article_category'] = $record->articleCategory->name;
+
     $edited_history = [
       'stakeholder_id' => auth()->id(),
-      'article_category_id' => $data['article_category_id'],
+      'article_category' => $data['article_category'],
       'title' => $data['title'],
       'slug' => $data['slug'],
       'content' => $data['content'],
       'thumbnail' => $data['thumbnail'],
       'thumbnail_alt' => $data['thumbnail_alt'],
       'images' => $data['images'],
+      'tags' => $data['tags'],
+      'modify_at' => Carbon::now()->toDateTimeString(),
     ];
 
     $getArticle = Published::where('main_article_id', $record->id)->first();
