cdceda8b110e3872133c7d3dec7c7c21ad6a695f
kamal <kamal.zyel@gmail.com>
2023-10-03 12:03:14 +0700

    artikel kategori deklarasi relasi

--- app/Models/ArticleCategory.php
+++ app/Models/ArticleCategory.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ArticleCategory extends Model
 {
-    use HasFactory, SoftDeletes;
+  use HasFactory, SoftDeletes;
 
-    protected $fillable = [
-        'slug', 'name', 'description'
-    ];
+  protected $fillable = [
+    'slug', 'name', 'description'
+  ];
+
+  public function mainArticle(): HasMany
+  {
+    return $this->hasMany(MainArticle::class);
+  }
 }
