1ec050a09ea5184b885a8853162b828788d1804d
kamal <kamal.zyel@gmail.com>
2023-10-03 12:00:09 +0700

    actions: - dynamic minDate and maxDate - save json for history fixed - infolist with custom component

--- app/Filament/Resources/MainArticleResource.php
+++ app/Filament/Resources/MainArticleResource.php
@@ -10,14 +10,11 @@ use Filament\Forms\Form;
 use Filament\Forms\Set;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Group;
 use Filament\Forms\Components\Radio;
-use Filament\Forms\Components\Tabs;
-use Filament\Forms\Components\Tabs\Tab;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
@@ -27,7 +24,9 @@ use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Carbon\Carbon;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\ViewEntry;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
@@ -41,148 +40,148 @@ class MainArticleResource extends Resource
   {
     return $form
       ->schema([
-        Tabs::make('Artikel')->tabs([
-          Tab::make('Formulir Artikel')->schema([
-            Group::make([
-              Section::make('Form Artikel')
-                ->description('Formulir artikel berita utama')
-                ->schema([
-                  TextInput::make('title')
-                    ->maxLength(256)
-                    ->dehydrated()
+        Group::make([
+          Section::make('Form Artikel')
+            ->description('Formulir artikel berita utama')
+            ->schema([
+              TextInput::make('title')
+                ->maxLength(256)
+                ->dehydrated()
+                ->required()
+                ->label('Judul berita')
+                ->live(onBlur: true)
+                ->afterStateUpdated(function (Set $set, ?string $state) {
+                  $set('slug', Str::slug($state));
+                })->placeholder('Judul berita/artikel')
+                ->autocomplete(false)
+                ->columnSpanFull(),
+              TextInput::make('slug')
+                ->required()
+                ->readOnly()
+                ->maxLength(270),
+              Select::make('article_category_id')
+                ->native('false')
+                ->required()
+                ->options(ArticleCategory::all()
+                  ->pluck('name', 'id')->map(function ($name) {
+                    return ucwords($name);
+                  }))
+                ->label('Kategori Berita'),
+              Select::make('tags')
+                ->label('Tags')
+                ->helperText('Max: 5 Tag')
+                ->multiple()
+                ->maxItems(5)
+                ->relationship('tags', 'name')
+                ->createOptionForm([
+                  TextInput::make('name')
+                    ->unique()
                     ->required()
-                    ->label('Judul berita')
+                    ->label('Tag')
+                    ->helperText('Max: 30 Karakter')
                     ->live(onBlur: true)
-                    ->afterStateUpdated(function (Set $set, ?string $state) {
-                      $set('slug', Str::slug($state));
-                    })->placeholder('Judul berita/artikel')
-                    ->autocomplete(false)
-                    ->columnSpanFull(),
-                  TextInput::make('slug')
-                    ->required()
-                    ->readOnly()
-                    ->maxLength(270)
-                    ->columnSpanFull(),
-                  Select::make('article_category_id')
-                    ->native('false')
-                    ->required()
-                    ->options(ArticleCategory::all()
-                      ->pluck('name', 'id')->map(function ($name) {
-                        return ucwords($name);
-                      }))
-                    ->label('Kategori Berita'),
-                  Select::make('tags')
-                    ->label('Tags')
-                    ->helperText('Max: 5 Tag')
-                    ->multiple()
-                    ->maxItems(5)
-                    ->relationship('tags', 'name')
-                    ->createOptionForm([
-                      TextInput::make('name')
-                        ->unique()
-                        ->required()
-                        ->label('Tag')
-                        ->helperText('Max: 30 Karakter')
-                        ->live(onBlur: true)
-                        ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state)))->placeholder('Tag')->autocomplete(false),
-                      TextInput::make('slug')->readOnly()->required(),
-                    ])
-                    ->createOptionAction(function (Forms\Components\Actions\Action $action) {
-                      return $action
-                        ->modalHeading('Create Tag')
-                        ->modalWidth('lg');
-                    }),
-                  TextInput::make('thumbnail_alt')
-                    ->required()
-                    ->maxLength(100)
-                    ->placeholder('Teks keterangan gambar')
-                    ->label('Keterangan gambar')
-                    ->helperText('Max: 100 Karakter')->columnSpanFull(),
-                  FileUpload::make('thumbnail')
-                    ->required()
-                    ->image()
-                    ->maxSize(1024)
-                    ->label('Thumbnail')
-                    ->helperText('Max Size: 1024KB'),
-                  FileUpload::make('images')
-                    ->maxFiles(5)
-                    ->image()
-                    ->multiple()
-                    ->reorderable()
-                    ->label('Kumpulan Gambar Artikel')
-                    ->helperText('Max: 5 File'),
-                  RichEditor::make('content')
-                    ->required()
-                    ->maxLength(5000)
-                    ->label('Kontent')->columnSpanFull(),
+                    ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state)))->placeholder('Tag')->autocomplete(false),
+                  TextInput::make('slug')->readOnly(),
                 ])
-                ->columns(2),
-
-              Section::make('Publikasi')
-                ->relationship('published')
-                ->description('Atur Publikasi Artikel')
-                ->schema([
-                  DatePicker::make('publish_date')
-                    ->required()
-                    ->native(false)
-                    ->minDate(today())
-                    ->disabledOn('edit')
-                    ->maxDate(Carbon::now()->addDays(3))
-                    ->default(today())
-                    ->displayFormat('D d/m/Y')
-                    ->label('Tanggal Publikasi')
-                    ->closeOnDateSelection(),
-                  Radio::make('edited_status')
-                    ->required()
-                    ->options([
-                      'drafted' => 'Draft',
-                      'completed' => 'Selesai',
-                      'archived' => 'Arsip',
-                    ])
-                    ->label('Status Edit')
-                    ->default('drafted'),
-
-                  Radio::make('publish_status')
-                    ->required()
-                    ->options([
-                      'queue' => 'Antrian',
-                      'preview' => 'Preview',
-                      'hold' => 'Tahan',
-                      'publish' => 'Publish'
-                    ])
-                    ->descriptions([
-                      'queue' => 'Artikel dalam antrian',
-                      'preview' => 'Artikel sedang di review',
-                      'hold' => 'Artikel ditahan sementara',
-                      'publish' => 'Artikel di publish'
-                    ])
-                    ->label('Status Publikasi')
-                    ->default('queue'),
-                ])
-                ->mutateRelationshipDataBeforeCreateUsing(function (array $data, Model $record): array {
-                  $edited_history = [
-                    'stakeholder_id' => auth()->id(),
-                    'article_category_id' => $record['id'],
-                    'title' => $record['title'],
-                    'slug' => $record['slug'],
-                    'content' => $record['content'],
-                    'thumbnail' => $record['thumbnail'],
-                    'thumbnail_alt' => $record['thumbnail_alt'],
-                    'images' => $record['images'],
-                  ];
-                  $data['edited_history'] = [$edited_history];
-                  $data['stakeholder_id'] = auth()->id();
-                  return $data;
+                ->createOptionAction(function (Forms\Components\Actions\Action $action) {
+                  return $action
+                    ->modalHeading('Create Tag')
+                    ->modalWidth('lg');
                 })
-                ->columns(3),
+                ->columnSpanFull(),
+              TextInput::make('thumbnail_alt')
+                ->required()
+                ->maxLength(100)
+                ->placeholder('Teks keterangan gambar')
+                ->label('Keterangan gambar')
+                ->helperText('Max: 100 Karakter')->columnSpanFull(),
+              FileUpload::make('thumbnail')
+                ->required()
+                ->image()
+                ->maxSize(1024)
+                ->label('Thumbnail')
+                ->helperText('Max Size: 1024KB'),
+              FileUpload::make('images')
+                ->maxFiles(5)
+                ->image()
+                ->multiple()
+                ->reorderable()
+                ->label('Kumpulan Gambar Artikel')
+                ->helperText('Max: 5 File'),
+              RichEditor::make('content')
+                ->required()
+                ->maxLength(5000)
+                ->label('Kontent')->columnSpanFull(),
+            ])->columns(2),
+
+        ])->columnSpan(['lg' => 2]),
+        Group::make([
+          Section::make('Publikasi')
+            ->relationship('published')
+            ->description('Atur Publikasi Artikel')
+            ->schema([
+              DateTimePicker::make('publish_date')
+                ->label('Tanggal Publikasi')
+                ->required()
+                ->seconds(false)
+                ->native(false)
+                ->default(today())
+                ->minDate(function ($get) {
+                  return $get('publish_date') ?: today();
+                })
+                ->maxDate(function ($get) {
+                  $date = $get('publish_date') ? Carbon::parse($get('publish_date')) : Carbon::now();
+                  return $date->addDays(3);
+                })
+                ->displayFormat('D d/m/Y')
+                ->closeOnDateSelection(),
+              Radio::make('edited_status')
+                ->required()
+                ->options([
+                  'drafted' => 'Draft',
+                  'completed' => 'Selesai',
+                  'archived' => 'Arsip',
+                ])
+                ->label('Status Edit')
+                ->default('drafted'),
+
+              Radio::make('publish_status')
+                ->required()
+                ->options([
+                  'queue' => 'Antrian',
+                  'preview' => 'Preview',
+                  'hold' => 'Tahan',
+                  'publish' => 'Publish'
+                ])
+                ->descriptions([
+                  'queue' => 'Artikel dalam antrian',
+                  'preview' => 'Artikel sedang di review',
+                  'hold' => 'Artikel ditahan sementara',
+                  'publish' => 'Artikel di publish'
+                ])
+                ->label('Status Publikasi')
+                ->default('queue'),
             ])
-              ->columnSpanFull(),
-          ]),
-          Tab::make('History')->schema([
-            // History Data
-          ])->hiddenOn('created')
-        ])->columnSpanFull()
-      ]);
+            ->mutateRelationshipDataBeforeCreateUsing(function (array $data, Model $record): array {
+              $tags = $record->tags->pluck('name')->filter();
+              $edited_history = [
+                'stakeholder_id' => auth()->id(),
+                'article_category' => $record->articleCategory->name,
+                'title' => $record->title,
+                'slug' => $record->slug,
+                'content' => $record->content,
+                'thumbnail' => $record->thumbnail,
+                'thumbnail_alt' => $record->thumbnail_alt,
+                'images' => $record->images,
+                'tags' => $tags,
+                'modify_at' => $record->created_at->toDateTimeString(),
+              ];
+              $data['edited_history'] = [$edited_history];
+              $data['stakeholder_id'] = auth()->id();
+              return $data;
+            }),
+        ])->columnSpan(['lg' => 1]),
+      ])->columns(3);
   }
 
   public static function infolist(Infolist $infolist): Infolist
@@ -191,28 +190,30 @@ class MainArticleResource extends Resource
       ->schema([
         ComponentsTabs::make('tabs')->schema([
           TabsTab::make('Informasi Artikel')->schema([
-            TextEntry::make('title'),
-            TextEntry::make('slug'),
+            TextEntry::make('articleCategory.name')->label('Kategori'),
             TextEntry::make('tags.name')->badge(),
-            ImageEntry::make('thumbnail')->label('Thumbnail'),
-            TextEntry::make('thumbnail_alt')->label('Keterangan Thumbnail'),
-            ImageEntry::make('images')->label('Thumbnail'),
-            TextEntry::make('content')->html(),
-          ]),
-          TabsTab::make('History')->schema([
-            TextEntry::make('published.edited_status')->label('Edited Status')->badge(),
-            TextEntry::make('published.publish_status')->label('Publish Status')->badge(),
-            TextEntry::make('published.publish_date')->label('Publish Date'),
-            TextEntry::make('published.edited_history')->label('History Edit')
-              ->state(function (Model $record): string {
-                $result = json_encode($record->published['edited_history']);
-                return $result;
-              })
-              ->columnSpanFull(),
+            TextEntry::make('title')->label('Judul Artikel'),
+            TextEntry::make('slug')->label('Slug Artikel'),
+            ImageEntry::make('thumbnail')->label('Gambar Cover'),
+            TextEntry::make('thumbnail_alt')->label('Keterangan Gambar Thumbnail'),
+            ImageEntry::make('images')->label('Kumpulan Gambar')->columnSpanFull(),
+            TextEntry::make('content')->html()->label('Konten')->columnSpanFull(),
+          ])->columns(2),
+          TabsTab::make('published')->label('Publikasi')->schema([
+            TextEntry::make('published.edited_status')->label('Status Pengeditan')->badge(),
+            TextEntry::make('published.publish_status')->label('Status Publikasi')->badge(),
+            TextEntry::make('published.publish_date')->label('Tanggal Publikasi'),
+            ViewEntry::make('published.edited_history')->view('livewire.collapsible-card')->columnSpanFull(),
           ])->columns(3),
-          TabsTab::make('Owner')->schema([
-            TextEntry::make('published.stakeholder_id'),
-          ]),
+
+          TabsTab::make('Author')->schema([
+            TextEntry::make('users.name')->label('Nama'),
+            TextEntry::make('institutes.name')->label('Instansi/OPD'),
+            TextEntry::make('stakeholders.level')->label('Level'),
+            TextEntry::make('stakeholders.position')->label('Posisi'),
+            TextEntry::make('stakeholders.photo')->label('Photo'),
+            TextEntry::make('stakeholders.description')->label('Bio'),
+          ])->columns(2),
         ])->columnSpanFull()
       ])->columns(2);
   }
