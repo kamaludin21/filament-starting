8186f5f0cc856cff8f9a18df6f648b4a8909ae61
kamal <kamal.zyel@gmail.com>
2023-10-03 12:03:28 +0700

    custom css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Filament\Support\Assets\Css;
+use Filament\Support\Facades\FilamentAsset;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+      FilamentAsset::register([
+        Css::make('custom-stylesheet', __DIR__ . '/../../resources/css/custom.css'),
+    ]);
     }
 }
