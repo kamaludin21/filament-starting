0507252f1427eebc0f13f738c78108a58c37e12d
kamal <kamal.zyel@gmail.com>
2023-10-09 15:01:08 +0700

    article seeder rename

--- database/seeders/ArticleCategorySeeder.php
+++ database/seeders/ArticleCategorySeeder.php
@@ -8,38 +8,38 @@ use Illuminate\Database\Seeder;
 
 class ArticleCategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        ArticleCategory::create([
-          'name' => 'politik dan pemerintahan',
-          'slug' => 'politik-dan-pemerintahan',
-        ]);
-        ArticleCategory::create([
-          'name' => 'sosial dan budaya',
-          'slug' => 'sosial-dan-budaya',
-        ]);
-        ArticleCategory::create([
-          'name' => 'pendidikan dan teknologi',
-          'slug' => 'pendidikan-dan-teknologi',
-        ]);
-        ArticleCategory::create([
-          'name' => 'olahraga',
-          'slug' => 'olahraga',
-        ]);
-        ArticleCategory::create([
-          'name' => 'lingkungan dan kesehatan',
-          'slug' => 'lingkungan-dan-kesehatan',
-        ]);
-        ArticleCategory::create([
-          'name' => 'transportasi',
-          'slug' => 'transportasi',
-        ]);
-        ArticleCategory::create([
-          'name' => 'religi',
-          'slug' => 'religi',
-        ]);
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    ArticleCategory::create([
+      'name' => 'Politik dan Pemerintahan',
+      'slug' => 'politik-dan-pemerintahan',
+    ]);
+    ArticleCategory::create([
+      'name' => 'Sosial dan Budaya',
+      'slug' => 'sosial-dan-budaya',
+    ]);
+    ArticleCategory::create([
+      'name' => 'Pendidikan dan Teknologi',
+      'slug' => 'pendidikan-dan-teknologi',
+    ]);
+    ArticleCategory::create([
+      'name' => 'Olahraga',
+      'slug' => 'olahraga',
+    ]);
+    ArticleCategory::create([
+      'name' => 'Lingkungan dan Kesehatan',
+      'slug' => 'lingkungan-dan-kesehatan',
+    ]);
+    ArticleCategory::create([
+      'name' => 'Transportasi',
+      'slug' => 'transportasi',
+    ]);
+    ArticleCategory::create([
+      'name' => 'Religi',
+      'slug' => 'religi',
+    ]);
+  }
 }
