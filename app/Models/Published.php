f66be34c762741fc0cc9dd917c93564789b3a85e
kamal <kamal.zyel@gmail.com>
2023-10-17 11:35:06 +0700

    cast enums for published mode

--- app/Models/Published.php
+++ app/Models/Published.php
@@ -2,31 +2,37 @@
 
 namespace App\Models;
 
+use App\Enums\EditedStatus;
+use App\Enums\PublishStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Published extends Model
 {
-    use HasFactory, SoftDeletes;
+  use HasFactory, SoftDeletes;
 
-    protected $fillable = [
-      'stakeholder_id',
-      'main_article_id',
-      'publish_status',
-      'publish_date',
-      'edited_status',
-      'edited_history'
-    ];
+  protected $fillable = [
+    'stakeholder_id',
+    'main_article_id',
+    'publish_status',
+    'publish_date',
+    'edited_status',
+    'edited_history'
+  ];
 
-    protected $casts = [
-      'edited_history' => 'array',
-      'publish_date' => 'datetime',
-    ];
+  protected $casts = [
+    'edited_history' => 'array',
+    'publish_date' => 'datetime',
+    'publish_status' => PublishStatus::class,
+    'edited_status' => EditedStatus::class,
+  ];
 
-    public function mainArticle(): BelongsTo
-    {
-      return $this->belongsTo(MainArticle::class);
-    }
+  public function mainArticle(): BelongsTo
+  {
+    return $this->belongsTo(MainArticle::class);
+  }
+
 }
