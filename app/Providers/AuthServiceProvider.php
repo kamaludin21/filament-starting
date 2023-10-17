713db8b58f3363295cfc04620c859506f741c046
kamal <kamal.zyel@gmail.com>
2023-10-17 11:29:14 +0700

    registers policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,24 +3,30 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\ContributorArticle;
+use App\Models\MainArticle;
+use App\Policies\ContributorArticlePolicy;
+use App\Policies\MainArticlePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
+  /**
+   * The model to policy mappings for the application.
+   *
+   * @var array<class-string, class-string>
+   */
+  protected $policies = [
+    MainArticle::class => MainArticlePolicy::class,
+    ContributorArticle::class => ContributorArticlePolicy::class,
+  ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+  /**
+   * Register any authentication / authorization services.
+   */
+  public function boot(): void
+  {
+    //
+  }
 }
